b7929ba09dab62cc4cf1bb708cc25a7a694daab0
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-25 22:12:46 +0200

    Use conan cmake

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ def get_version():
     git = tools.Git()
     try:
         tag = git.get_tag()
-        return tag if tag else "4.3.0"
+        return tag if tag else "1.0.0"
     except:
         return None
 
@@ -20,18 +20,19 @@ class DemoConan(ConanFile):
     exports_sources = ["CMakeLists.txt", "src/*"]
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/0.1@%s/stable" % self.user)
+        self.build_requires("cmake/3.15.3@%s/stable" % self.user)
 
     def build(self):
-        cmake = CMake(self)
+        cmake = CMake(self, generator="Ninja")
         cmake.configure()
         cmake.install()
 
     def package(self):
         if self.settings.build_type == "Debug":
-            self.copy("*.c*", "src")
-            self.copy("*.h*", "src")
+            self.copy("*.cpp", "src")
+            self.copy("*.hpp", "src")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
