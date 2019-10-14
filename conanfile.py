2a8743eb113af0ec8cdb36e1de43bb8cd2c4012a
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-14 15:15:32 +0200

    Add both meson and cmake as option

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,33 @@
-from conans import ConanFile, tools, CMake
+from conans import CMake, ConanFile, Meson, tools
+
 
 class DemoConan(ConanFile):
     name = "demo"
-    version = os.environ["CI_COMMIT_REF_NAME"]
+    version = tools.get_env("GIT_TAG", "1.0.0")
     url = "http://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
-    description = ("Demo conan package")
+    description = "Demo conan package"
     settings = "os", "arch", "compiler", "build_type"
-    exports_sources = ["CMakeLists.txt", "src/*"]
+    exports_sources = ("meson.build", "CMakeLists.txt", "src/*")
+    options = {"build_system": ["meson", "cmake"]}
+    default_options = "build_system=meson"
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
+        if self.options.build_system == "meson":
+            self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
+        elif self.options.build_system == "cmake":
+            self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def build(self):
-        cmake = CMake(self, generator="Ninja")
-        cmake.configure()
-        cmake.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.cpp", "src")
-            self.copy("*.hpp", "src")
+        if self.options.build_system == "meson":
+            meson = Meson(self)
+            meson.configure(build_folder="build")
+            meson.install()
+        elif self.options.build_system == "cmake":
+            cmake = CMake(self, generator="Ninja")
+            cmake.configure()
+            cmake.install()
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "src.hpp"
 #include "gen.hpp"
 
+#include <math.h>
+
 int main(void)
 {
     src_func();
     gen_func();
+    pow(2, 2);
     return 0;
 }
